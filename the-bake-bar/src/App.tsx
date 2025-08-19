ee0d172c7c77ccfaaf6cd72077220d7c02bf1d0b
Jagan Langa Samydurai <jlanga@logitech.com>
2025-08-19 21:44:34 +0530

    Fix React Router basename for GitHub Pages deployment

--- the-bake-bar/src/App.tsx
+++ the-bake-bar/src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
   return (
     <ThemeProvider>
       <CartProvider>
-        <Router>
+        <Router basename="/the-bake-bar">
           <div className="page-container">
             <Navbar />
             <main>
