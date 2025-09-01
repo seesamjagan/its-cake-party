16949645589c4e70c0db8ad187edfa8131923080
Jagan Langa Samydurai <jlanga@logitech.com>
2025-09-01 17:08:52 +0530

    update base name for router

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
   return (
     <ThemeProvider>
       <CartProvider>
-        <Router basename="/its-cake-party">
+        <Router>
           <div className="page-container">
             <Navbar />
             <main>
